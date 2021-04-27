666e490204ef557027320f27054f0188146c5457
daluozha <561890199@qq.com>
2021-04-27 11:31:34 +0800

    edit vue.config.js to close the eslint

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+// vue.config.js
+module.exports = {
+    devServer: {
+      overlay: {
+        warnings: false, //不显示警告
+        errors: false	//不显示错误
+      }
+    },
+    lintOnSave:false //关闭eslint检查
+  }
+  
